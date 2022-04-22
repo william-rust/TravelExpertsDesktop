f796b472f7889e7f8400dd5e9d9a5df879df3b21
William Rust <william_rust@outlook.com>
2022-04-21 18:44:00 -0600

    Adjusted dates on table, minor table spacing changes. Split Postal code from the rest of address.

--- team4_desktop/src/main/java/desktop/team4_desktop/MainController.java
+++ team4_desktop/src/main/java/desktop/team4_desktop/MainController.java
@@ -313,7 +313,7 @@ public class MainController {
                 custDB.add(new Customer(rs.getInt(1), rs.getString(2), rs.getString(3),
                         rs.getString(4), rs.getString(5), rs.getString(6),
                         rs.getString(7), rs.getString(8), rs.getString(9),
-                        rs.getString(10), rs.getString(11).trim(), rs.getInt(12)));
+                        rs.getString(10), rs.getString(11).trim(), rs.getInt(12)));  // Trim in entry #11 to clear the whitespace on some email addresses
             }
             conn.close();
 //        } catch (ClassNotFoundException e){
